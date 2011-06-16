fa9dfb56233310d68896c598618686ed871afbde
Neil Singer <neiljsinger@yahoo.com>
2011-06-15 21:53:00 -0400

    initial crazy commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crazyapp::Application.config.secret_token = '3801cc1dedb3d2dd3d02cd3ce985cfebd9e92f364935e309373ce44ee2178bd74126e1d3aaa5904ff471ef00ef85541edd186bda7388f14bbf9bc105e71c0659'
